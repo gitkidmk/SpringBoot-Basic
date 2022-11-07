a0da03aab6fe1ab2134bc6f46453225ea83a4891
gitkidmk <lovekangmk@gmail.com>
2022-11-07 23:25:56 +0900

    [feat] AppConfig를 통한 관심사 분리
    - 구현체가 인터페이스 외에 또다른 구현체에 묶여있지 않도록 수정
    - AppConfig 구체 클래스를 선택하는 역할을 담당하도록 수정
    - DIP 지키도록 수정됨

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     // TODO: why final?
     // 인터페이스에 대한 구현체가 하나인 경우에는 Impl이라고 명시
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
         memberRepository.save(member);
